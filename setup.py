9c20ebd3f92639b26a825b703cbd5df8bc9ca8bf
Niko Wilbert <niko.wilbert@tngtech.com>
2018-08-17 14:38:11 +0200

    updated dependencies (especially jira, for Python 3.7 compatibility)

--- setup.py
+++ setup.py
@@ -42,12 +42,12 @@ setup(
     ],
     install_requires=[
         'click>=6.7',
-        'click-log>=0.2.1',
+        'click-log>=0.3.2',
         'colorama>=0.3.9',
-        'keyring>=10.5.0',
-        'jira>=1.0.10',
-        'networkx>=2.0',
-        'graphviz>=0.8.1',
+        'keyring>=13.2.1',
+        'jira>=2.0.0',
+        'networkx>=2.1',
+        'graphviz>=0.8.4',
     ],
     entry_points='''
         [console_scripts]
