e31b7e81eb2478f63ba20393f7eda5fe3018ff84
Achim Herwig <achim.herwig@tngtech.com>
2022-03-25 20:34:31 +0100

    Avoid crash when no sprint can be found.
    Signed-off-by: Achim Herwig <achim.herwig@tngtech.com>

--- src/jiradeps/jirawrapper.py
+++ src/jiradeps/jirawrapper.py
@@ -101,8 +101,11 @@ def get_sprints(issue: Issue) -> Optional[Set[str]]:
     sprint_field = _get_customfields().get('sprint')
     if not sprint_field:
         return None
-    return {SPRINT_ID_REGEX.search(sprint_raw).group(1)
-            for sprint_raw in getattr(issue.fields, sprint_field) or []}
+    return {
+        s.group(1)
+        for s in (SPRINT_ID_REGEX.search(sprint_raw) for sprint_raw in getattr(issue.fields, sprint_field))
+        if s is not None
+    }
 
 
 def get_story_points(issue: Issue) -> Optional[float]:
